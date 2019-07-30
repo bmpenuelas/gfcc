5d8bfce68c6ffc184dde36fcb40534a6be790660
Borja Penuelas <bmpenuelas@gmail.com>
2019-07-30 20:45:14 +0200

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
 
 setup(
     name='gfcc',
-    version='0.1',
+    version='0.2',
     description='A layer of automation for ClearCase with a touch of git flavour',
     long_description=readme(),
     classifiers=[
